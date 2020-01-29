0f75c4e18c32c1e997906ecc764590854ba1f920
Kevin Kamps <k.kamps@tfe.nl>
2020-01-29 13:15:18 +0100

    Fixed variable replacing in the hcl template

--- main.go
+++ main.go
@@ -64,12 +64,15 @@ func main() {
 	updateJob(*nomadUrl, contents)
 }
 
-func updateJob(nomadUrl string, contents []byte) {
+//Updates a job in nomad
+//Parameter nomadUrl	The url of nomad which to target
+//Parameter hclContent	The content of a hcl formatted file
+func updateJob(nomadUrl string, hclContent []byte) {
 	var jobFull map[string]interface{}
-	json.Unmarshal(contents, &jobFull)
+	json.Unmarshal(hclContent, &jobFull)
 
 	job := new(Job)
-	json.Unmarshal(contents, &job)
+	json.Unmarshal(hclContent, &job)
 
 	message := map[string]interface{}{
 		"Job": jobFull,
@@ -91,12 +94,55 @@ func updateJob(nomadUrl string, contents []byte) {
 	}
 }
 
+//Replaces variables in the content. Currently it only replaces environment variables.
 func replaceVars(content []byte) string {
 	contentString := string(content)
 	for _, e := range os.Environ() {
 		pair := strings.SplitN(e, "=", 2)
-		contentString = strings.Replace(contentString, "$"+pair[0], pair[1], -1)
+
+		searchKey := "$" + pair[0]
+		i := strings.Index(contentString, searchKey)
+		for i >= 0 {
+			contentString, i = replaceString(contentString, searchKey, pair[1], i)
+
+			// check if there is a next match
+			newIndex := strings.Index(contentString[i+1:], searchKey)
+			if newIndex == -1 {
+				i = newIndex
+				continue
+			}
+			i = i + 1 + strings.Index(contentString[i+1:], searchKey)
+		}
 	}
 
 	return contentString
+}
+
+//Replaces the old with the new in the content string. Only replaces the first find.
+//Parameter content 	The contents of the string in which to replace the old with the new
+//Parameter old			Will be replaced with the new
+//Parameter new			Used to replace the old
+//Parameter fromIndex	The index from which to start looking
+//return string 		Content with the replaced value
+//return int 			The value of the index of the new content replaced value | the original fromIndex
+func replaceString(content string, old string, new string, fromIndex int) (string, int) {
+	start := strings.Index(content[fromIndex:], old)
+	if start == -1 {
+		return content, fromIndex
+	}
+
+	var end int
+	for end = start + 1; end < len(content[fromIndex:]) && isShellVariableNameCharacter(content[fromIndex:][end]); end++ {
+	}
+	if len(old) != end-start {
+		return content, fromIndex
+	}
+
+	return content[0:fromIndex] + new + content[fromIndex+end:], fromIndex + end
+}
+
+//Checks if a certain character is valid for a shell variable name.
+//Parameter hclContent	The content of a hcl formatted file
+func isShellVariableNameCharacter(c uint8) bool {
+	return c == '_' || '0' <= c && c <= '9' || 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z'
 }
